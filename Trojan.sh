daf67ed2110d27662f57de091f9282152e1209b2
NymouseX <saibaougokasu@gmail.com>
2021-11-03 17:06:23 +0700

    TrojanFake

---
+++ Trojan.sh
@@ -0,0 +1,266 @@
+# buat password
+clear
+echo "Login Dulu Soalnya Bahaya" | lolcat
+sleep 1
+echo "***********************NYX*********************************"|lolcat
+echo "Author By   : Nymousex"|lolcat
+echo "My Team     : Cyber Hanter Exploit"|lolcat
+echo "My Youtube  : Nymousex" | lolcat
+echo "***********************NYX*********************************"|lolcat
+qu="Selamat Datang" 
+	read -p "Masukan Nama Anda :" NM;
+	read -p "Masukan Password Anda :" pass;
+	sleep 0.1
+echo
+echo $qu $NM | lolcat
+	sleep 0.1
+#kondisi
+if [ $pass = "Trojan" ]
+	then 
+	echo 
+	echo "Password Benar" | lolcat
+	sleep 0.1
+	else
+	echo "Password Salah" | lolcat
+	echo "Cek Youtube NymouseX" | lolcat
+	sleep 3
+	bash Trojan.sh
+	sleep 0.1
+fi
+#Trojan
+clear
+echo "
+████████╗██████╗░░█████╗░░░░░░██╗░█████╗░███╗░░██╗
+╚══██╔══╝██╔══██╗██╔══██╗░░░░░██║██╔══██╗████╗░██║
+░░░██║░░░██████╔╝██║░░██║░░░░░██║███████║██╔██╗██║
+░░░██║░░░██╔══██╗██║░░██║██╗░░██║██╔══██║██║╚████║
+░░░██║░░░██║░░██║╚█████╔╝╚█████╔╝██║░░██║██║░╚███║
+░░░╚═╝░░░╚═╝░░╚═╝░╚════╝░░╚════╝░╚═╝░░╚═╝╚═╝░░╚══╝" | lolcat
+echo "Pilih Teknik Haking"
+echo
+echo "1.Hack Via Nomor" | lolcat
+echo "2.Hack Via Email" | lolcat
+read -p "|$NM}--> :" JWB
+
+if [ $JWB = 1 ] || [ $JWB = 1 ]
+then 
+clear
+echo "***********************$NM*********************************"|lolcat
+echo "Author By   : Nymousex"|lolcat
+echo "My Team     : Cyber Hanter Exploit"|lolcat
+echo "My Youtube  : Nymousex" | lolcat
+echo "***********************$NM*********************************"|lolcat
+sleep 1
+echo "Masukan Nomor Korban :" | lolcat
+read -p "$NM $ :" KBN 
+echo
+echo "Pengiriman Trojan Sedang Dilakukan" | lolcat
+echo "[HACK>                      KORBAN]"
+sleep 0.1
+clear
+echo "[HACK>>                     KORBAN]"
+sleep 0.1
+clear
+echo "[HACK>>>                    KORBAN]"
+sleep 0.1
+clear
+echo "[HACK>>>>                   KORBAN]"
+sleep 0.1
+clear
+echo "[HACK>>>>>                  KORBAN]"
+sleep 0.1
+clear
+echo "[HACK>>>>>>                 KORBAN]"
+sleep 0.1
+clear
+echo "[HACK>>>>>>>                KORBAN]"
+sleep 0.1
+clear
+echo "[HACK>>>>>>>>               KORBAN]"
+sleep 0.1
+clear
+echo "[HACK>>>>>>>>>              KORBAN]"
+sleep 0.1
+clear
+echo "[HACK>>>>>>>>>>             KORBAN]"
+sleep 0.1
+clear
+echo "[HACK>>>>>>>>>>>            KORBAN]"
+sleep 0.1
+clear
+echo "[HACK>>>>>>>>>>>>           KORBAN]"
+sleep 0.1
+clear
+echo "[HACK>>>>>>>>>>>>>          KORBAN]"
+sleep 0.1
+clear
+echo "[HACK>>>>>>>>>>>>>>         KORBAN]"
+sleep 0.1
+clear
+echo "[HACK>>>>>>>>>>>>>>>        KORBAN]"
+sleep 0.1
+clear
+echo "[HACK>>>>>>>>>>>>>>>>       KORBAN]"
+sleep 0.1
+clear
+echo "[HACK>>>>>>>>>>>>>>>>>      KORBAN]"
+sleep 0.1
+clear
+echo "[HACK>>>>>>>>>>>>>>>>>>     KORBAN]"
+sleep 0.1
+clear
+echo "[HACK>>>>>>>>>>>>>>>>>>>    KORBAN]"
+sleep 0.1
+clear
+echo "[HACK>>>>>>>>>>>>>>>>>>>>   KORBAN]"
+sleep 0.1
+clear
+echo "[HACK>>>>>>>>>>>>>>>>>>>>>  KORBAN]"
+sleep 0.1
+clear
+echo "[HACK>>>>>>>>>>>>>>>>>>>>>> KORBAN]"
+sleep 0.1
+clear
+echo "[HACK>>>>>>>>>>>>>>>>>>>>>>>KORBAN]"
+sleep 0.1
+clear
+echo "Trojan Berhasih Dikirim ke $KBN"
+echo "Trojan Berhasih Dikirim ke $KBN"
+echo "Trojan Berhasih Dikirim ke $KBN"
+echo "Trojan Berhasih Dikirim ke $KBN"
+echo "Trojan Berhasih Dikirim ke $KBN"
+echo "Trojan Berhasih Dikirim ke $KBN"
+echo "Trojan Berhasih Dikirim ke $KBN"
+echo "Trojan Berhasih Dikirim ke $KBN"
+echo "Trojan Berhasih Dikirim ke $KBN"
+echo "Trojan Berhasih Dikirim ke $KBN"
+echo "Trojan Berhasih Dikirim ke $KBN"
+echo "Trojan Berhasih Dikirim ke $KBN"
+echo "Trojan Berhasih Dikirim ke $KBN"
+echo "Trojan Berhasih Dikirim ke $KBN"
+echo "Trojan Berhasih Dikirim ke $KBN"
+echo "Trojan Berhasih Dikirim ke $KBN"
+echo "Trojan Berhasih Dikirim ke $KBN"
+echo "Trojan Berhasih Dikirim ke $KBN"
+fi
+
+if [ $JWB = 2 ] || [ $JWB = 2 ]
+then 
+clear
+echo "***********************$NM*********************************"|lolcat
+echo "Author By   : Nymousex"|lolcat
+echo "My Team     : Cyber Hanter Exploit"|lolcat
+echo "My Youtube  : Nymousex" | lolcat
+echo "***********************$NM*********************************"|lolcat
+sleep 1
+echo
+echo "Masukan Email Korban :" | lolcat
+read -p "$NM $ :" KXN 
+echo
+echo "Pengiriman Trojan Sedang Dilakukan" | lolcat
+echo "[HACK>                      KORBAN]"
+sleep 0.1
+clear
+echo "[HACK>>                     KORBAN]"
+sleep 0.1
+clear
+echo "[HACK>>>                    KORBAN]"
+sleep 0.1
+clear
+echo "[HACK>>>>                   KORBAN]"
+sleep 0.1
+clear
+echo "[HACK>>>>>                  KORBAN]"
+sleep 0.1
+clear
+echo "[HACK>>>>>>                 KORBAN]"
+sleep 0.1
+clear
+echo "[HACK>>>>>>>                KORBAN]"
+sleep 0.1
+clear
+echo "[HACK>>>>>>>>               KORBAN]"
+sleep 0.1
+clear
+echo "[HACK>>>>>>>>>              KORBAN]"
+sleep 0.1
+clear
+echo "[HACK>>>>>>>>>>             KORBAN]"
+sleep 0.1
+clear
+echo "[HACK>>>>>>>>>>>            KORBAN]"
+sleep 0.1
+clear
+echo "[HACK>>>>>>>>>>>>           KORBAN]"
+sleep 0.1
+clear
+echo "[HACK>>>>>>>>>>>>>          KORBAN]"
+sleep 0.1
+clear
+echo "[HACK>>>>>>>>>>>>>>         KORBAN]"
+sleep 0.1
+clear
+echo "[HACK>>>>>>>>>>>>>>>        KORBAN]"
+sleep 0.1
+clear
+echo "[HACK>>>>>>>>>>>>>>>>       KORBAN]"
+sleep 0.1
+clear
+echo "[HACK>>>>>>>>>>>>>>>>>      KORBAN]"
+sleep 0.1
+clear
+echo "[HACK>>>>>>>>>>>>>>>>>>     KORBAN]"
+sleep 0.1
+clear
+echo "[HACK>>>>>>>>>>>>>>>>>>>    KORBAN]"
+sleep 0.1
+clear
+echo "[HACK>>>>>>>>>>>>>>>>>>>>   KORBAN]"
+sleep 0.1
+clear
+echo "[HACK>>>>>>>>>>>>>>>>>>>>>  KORBAN]"
+sleep 0.1
+clear
+echo "[HACK>>>>>>>>>>>>>>>>>>>>>> KORBAN]"
+sleep 0.1
+clear
+echo "[HACK>>>>>>>>>>>>>>>>>>>>>>>KORBAN]"
+sleep 0.1
+clear
+echo "Trojan Berhasih Dikirim ke $KXN"
+echo "Trojan Berhasih Dikirim ke $KXN"
+echo "Trojan Berhasih Dikirim ke $KXN"
+echo "Trojan Berhasih Dikirim ke $KXN"
+echo "Trojan Berhasih Dikirim ke $KXN"
+echo "Trojan Berhasih Dikirim ke $KXN"
+echo "Trojan Berhasih Dikirim ke $KXN"
+echo "Trojan Berhasih Dikirim ke $KXN"
+echo "Trojan Berhasih Dikirim ke $KXN"
+echo "Trojan Berhasih Dikirim ke $KXN"
+echo "Trojan Berhasih Dikirim ke $KXN"
+echo "Trojan Berhasih Dikirim ke $KXN"
+echo "Trojan Berhasih Dikirim ke $KXN"
+echo "Trojan Berhasih Dikirim ke $KXN"
+echo "Trojan Berhasih Dikirim ke $KXN"
+echo "Trojan Berhasih Dikirim ke $KXN"
+echo "Trojan Berhasih Dikirim ke $KXN"
+echo "Trojan Berhasih Dikirim ke $KXN"
+echo "Trojan Berhasih Dikirim ke $KXN"
+echo "Trojan Berhasih Dikirim ke $KXN"
+echo "Trojan Berhasih Dikirim ke $KXN"
+fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
